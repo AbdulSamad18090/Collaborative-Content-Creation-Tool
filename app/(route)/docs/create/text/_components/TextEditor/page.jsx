cc2460d1152857bd4221d08378a5ce95061ddf98
AbdulSamad18090 <abdulsamad18090@gmail.com>
2024-10-11 01:04:40 +0500

    created text editor with react quill

--- app/(route)/docs/create/text/_components/TextEditor/page.jsx
+++ app/(route)/docs/create/text/_components/TextEditor/page.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // Import the styles for Quill
 
-const TextEditor = () => {
+const RichTextEditor = () => {
+  const [value, setValue] = useState('');
+
+  const modules = {
+    toolbar: [
+      [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ 'script': 'sub'}, { 'script': 'super' }],
+      [{ 'indent': '-1'}, { 'indent': '+1' }, { 'align': [] }],
+      ['link', 'image'],
+      ['clean']                                         
+    ]
+  };
+
+  const formats = [
+    'header', 'font', 'size',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image', 'align', 'script'
+  ];
+
   return (
     <div>
-      Text Editor
+      <ReactQuill 
+        theme="snow" 
+        value={value} 
+        onChange={setValue} 
+        modules={modules} 
+        formats={formats} 
+        placeholder="Compose something awesome..."
+      />
     </div>
-  )
-}
+  );
+};
 
-export default TextEditor
+export default RichTextEditor;
